5f99dff2d20c20ed3aee2611619ef158eba004aa
Aman <158808544+AmanX0004@users.noreply.github.com>
2024-12-11 11:12:04 +0530

    Add files via upload

---
+++ InputExercise02-sep.py
@@ -0,0 +1,73 @@
+#__________________  1  _____________________
+# print("Find the area of Square.")
+# a = int(input("Enter The Side Of The Square"))
+# b = a*a
+# print("Area of square is ", b ) 
+#__________________  2  _____________________
+
+# print("Find the average of 3 person's age")
+
+# a1 =int(input("Enter 1st Person's Age"))
+# a2 =int(input("Enter 2nd Person's Age"))
+# a3 =int(input("Enter 3rd Person's Age"))
+
+# s = a1+a2+a3
+# av = s/3
+
+# print("Average of 3 person's age = " , av)
+
+#__________________  3  _____________________
+
+# print("Convert Dollar into Rupees")
+
+# d = float(input("Enter N0. of dollars "))
+
+# b = d*83.88
+
+# print(d," Dollar become" , b,"rupess"  )
+
+#__________________  4  _____________________
+
+# print("Convert Rupees into Dollar ")
+
+# r = float(input("Enter the number of rupess"))
+
+# b = r/83.88
+
+# print(r , "Rupees become" , b, "Dollars")
+
+
+#__________________  5  _____________________
+
+# print("Convert minute into second")
+
+# m = float(input("Enter Minutes"))
+
+# s = m*60
+
+# print(m ,"Minute become",s,"Second" )
+
+#__________________  6  _____________________
+
+# print("Convert second into minutes")
+
+# s = float(input("Enter Seconds"))
+
+# m = s/60
+# print(s,"seconds become " ,m,"minute" )
+
+#__________________  7  _____________________
+
+print("Find the area of rectangle")
+ 
+l = float(input("Enter the lenght of rectangle :"))
+w = float(input("Enter the width of rectangle :"))
+
+a = l*w
+
+print("Area of rectangle is " , a)
+
+
+
+
+
